802383f7de9e23fd0ab8ad2d214b6a49b7c62a6e
Wyndale <lapurawendel.dev@gmail.com>
2025-12-28 17:58:09 +0800

    chore: update database configuration for production environment

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$host = 'localhost';
-$dbname = 'greenroots_db';
-$username = 'root'; // Default XAMPP username
-$password = ')l6..O[btkCXpBWd';
+$host = 'sql211.infinityfree.com';
+$dbname = 'if0_40752096_greenroots_db';
+$username = 'if0_40752096';
+$password = 'BKhAGIbyv6';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
